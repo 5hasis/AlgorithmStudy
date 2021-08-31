62058e235b0a913481b2ad4c97879340873e0289
5hasis <dhgkdud4002@naver.com>
2021-08-31 14:36:41 +0900

    스택 수열

---
+++ HEM_python/Stack_/StackSequence.py
@@ -0,0 +1,45 @@
+#1874
+
+n = int(input())
+arr = list()
+
+seq = []
+for _ in range(n):
+    seq.append(int(input()))
+
+tmp = []
+res = []
+
+j=0
+i=0
+
+while(True):
+    
+    if tmp:
+        if seq[j]==tmp[-1]:
+            arr.append('-')
+            res.append(tmp[-1])
+            tmp.pop()
+            j+=1
+            #print('tmp : ',tmp)
+            #print('res : ',res)
+            continue
+    
+    i += 1
+    #print("i : ",i)
+
+    if i>n:
+        break
+
+    tmp.append(i)
+    #print('tmp : ',tmp)
+    #print(res)
+    arr.append('+')
+    
+    
+if seq == res:
+    for i in range(len(arr)):
+        print(arr[i])
+else:
+    print("NO")
+            
